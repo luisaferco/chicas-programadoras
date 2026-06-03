48b8d248cbe33a28d67d99010da9533f363927e4
luisa.colorado <luisa.colorado.e@gmail.com>
2026-06-03 18:33:36 -0500

    remove event

--- form-page/main.js
+++ form-page/main.js
@@ -3,30 +3,3 @@ const imgElement = document.getElementById("cat-image");
 
 // use a default cat image before the user submits the form
 document.getElementById("cat-image").src = "https://cdn2.thecatapi.com/images/2h1.jpg";
-
-
-form.addEventListener("submit", async function(event) {
-
-  // Prevent page reload
-  event.preventDefault();
-
-  // GET random cat image
-  const response = await fetch(
-    "https://api.thecatapi.com/v1/images/search"
-  );
-
-  const data = await response.json();
-
-  const catImage = data[0].url;
-
-document.getElementById("cat-image").src = catImage;
-
-  previewCard.innerHTML = `
-  
-    <img
-      src="${catImage}"
-      alt="Cute cat"
-    >   
-
-  `;
-});
